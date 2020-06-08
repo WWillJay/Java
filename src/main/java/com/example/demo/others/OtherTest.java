3d9ad4358fedd5bbd915acb37d0643b07fb304a6
wuweijie <wuweijie>
2020-06-08 09:44:26 +0800

    其他测试

--- src/main/java/com/example/demo/others/OtherTest.java
+++ src/main/java/com/example/demo/others/OtherTest.java
@@ -1,12 +1,21 @@
 package com.example.demo.others;
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 
 public class OtherTest {
 
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, -5);
-        System.out.println(calendar.get(Calendar.DATE));
+//        Calendar calendar = Calendar.getInstance();962464
+
+//        calendar.add(Calendar.DATE, -5);
+//        System.out.println(calendar.get(Calendar.DATE));
+
+        BigDecimal a = new BigDecimal(1);
+        BigDecimal b = new BigDecimal(3);
+        BigDecimal c = a.divide(b).setScale(3,BigDecimal.ROUND_DOWN);
+//        c.setScale(2, BigDecimal.ROUND_HALF_UP);
+        System.out.println(c);
+
     }
 }
